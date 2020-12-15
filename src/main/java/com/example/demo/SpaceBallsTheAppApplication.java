386ed2a561ca94af135ff5f94970ac61857800be
shortf15e <73486744+shortf15e@users.noreply.github.com>
2020-12-15 12:08:47 -0800

    Initial commit

---
+++ src/main/java/com/example/demo/SpaceBallsTheAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceBallsTheAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceBallsTheAppApplication.class, args);
+	}
+
+}
