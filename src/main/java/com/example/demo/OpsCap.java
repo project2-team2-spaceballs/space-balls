03ae309207e9a2020e94f6885fc2dbda578a9d61
Avery Merriex <averymerriex@gmail.com>
2020-12-16 15:06:39 -0500

    added post mapping

--- src/main/java/com/example/demo/OpsCap.java
+++ src/main/java/com/example/demo/OpsCap.java
@@ -1,2 +1,70 @@
-package com.example.demo;public class OpsCap {
+package com.example.demo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "opscap")
+public class OpsCap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long radar_id;
+
+    private Long mw_stat;
+
+    private Long md_stat;
+
+    private  Long sda_stat;
+
+    private String details;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRadar_id() {
+        return radar_id;
+    }
+
+    public void setRadar_id(Long radar_id) {
+        this.radar_id = radar_id;
+    }
+
+    public Long getMw_stat() {
+        return mw_stat;
+    }
+
+    public void setMw_stat(Long mw_stat) {
+        this.mw_stat = mw_stat;
+    }
+
+    public Long getMd_stat() {
+        return md_stat;
+    }
+
+    public void setMd_stat(Long md_stat) {
+        this.md_stat = md_stat;
+    }
+
+    public Long getSda_stat() {
+        return sda_stat;
+    }
+
+    public void setSda_stat(Long sda_stat) {
+        this.sda_stat = sda_stat;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
 }
--- src/main/java/com/example/demo/OpsCapController.java
+++ src/main/java/com/example/demo/OpsCapController.java
@@ -1,2 +1,27 @@
-package com.example.demo;public class OpsCapController {
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/opscap")
+public class OpsCapController {
+
+    private final OpscapRepo repository;
+
+    public OpsCapController(OpscapRepo repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public Iterable<OpsCap> all() {
+        return this.repository.findAll();
+    }
+
+    @PostMapping("")
+    public OpsCap create(@RequestBody OpsCap opsCap) {
+        return this.repository.save(opsCap);
+
+    }
+
+
 }
