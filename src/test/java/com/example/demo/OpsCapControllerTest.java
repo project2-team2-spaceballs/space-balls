25cd04e7b6e6805ffbf5369c779a229ed5fa7b16
Avery Merriex <averymerriex@gmail.com>
2020-12-17 11:32:04 -0500

    updated post request

--- src/test/java/com/example/demo/OpsCapControllerTest.java
+++ src/test/java/com/example/demo/OpsCapControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,12 +17,14 @@ import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 
 
 import javax.transaction.Transactional;
+import java.util.HashMap;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -55,6 +58,34 @@ public class OpsCapControllerTest {
         this.mvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].details", is("Testing")));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testPostOpsCapStatus() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        HashMap<String, Object> newOpsCap = new HashMap<>() {
+            {
+                put("radar_id", 4);
+                put("mw_stat", 3);
+                put("md_stat", 3);
+                put("sda_stat", 2);
+                put("details", "Please let this work");
+            }
+        };
+
+        String json = objectMapper.writeValueAsString(newOpsCap);
+
+        MockHttpServletRequestBuilder request = post("/opscap")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", instanceOf(Number.class)))
+                .andExpect(jsonPath("$.details", is("Please let this work")));
+
     }
 
 
--- src/test/java/com/example/demo/OpsCapStatusControllerTest.java
+++ src/test/java/com/example/demo/OpsCapStatusControllerTest.java
@@ -1,4 +1,59 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Role;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import javax.transaction.Transactional;
+
+import java.util.HashMap;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class OpsCapStatusControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    OpsCapStatusRep repository;
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testGetOpsCapStatus() throws Exception {
+
+        OpsCapStatus opsCapStatus = new OpsCapStatus();
+
+        opsCapStatus.setColor("red");
+        opsCapStatus.setId(3l);
+
+        repository.save(opsCapStatus);
+
+        MockHttpServletRequestBuilder request = get("/opscapstatus")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].color", is("red")));
+
+    }
+
+
+
+
 }
