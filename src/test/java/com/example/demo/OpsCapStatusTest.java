7fac66e39bc239a7861d4b676b54c5fc9588320d
Avery Merriex <averymerriex@gmail.com>
2020-12-17 10:22:49 -0500

    updated ops cap status test

--- src/test/java/com/example/demo/OpsCapStatusTest.java
+++ src/test/java/com/example/demo/OpsCapStatusTest.java
@@ -1,4 +1,32 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class OpsCapStatusTest {
+
+    OpsCapStatus opsCapStatus;
+
+    @BeforeEach
+    public void setup() {
+        opsCapStatus = new OpsCapStatus();
+    }
+
+    @Test
+    public void testsetId() {
+        opsCapStatus.setId(3L);
+        long results = opsCapStatus.getId();
+
+        assertEquals(3L, results);
+    }
+
+    @Test
+    public void testsetcolot() {
+        opsCapStatus.setColor("red");
+        String results = opsCapStatus.getColor();
+
+        assertEquals("red", results);
+    }
 }
